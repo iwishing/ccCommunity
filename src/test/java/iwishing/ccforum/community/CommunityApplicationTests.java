ea87f18087edf9a0cfdc4028511cdbfa11def482
Void <35948348+1223372496@users.noreply.github.com>
2020-04-17 13:05:22 +0800

    init repo

---
+++ src/test/java/iwishing/ccforum/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package iwishing.ccforum.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

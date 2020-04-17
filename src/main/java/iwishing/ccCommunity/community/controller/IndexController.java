310ae2d71cf7e35a6a0fde2a6febe8f303a7d854
iwishing <cbwen1997@foxmail.com>
2020-04-17 15:41:16 +0800

    test commit

--- src/main/java/iwishing/ccCommunity/community/controller/IndexController.java
+++ src/main/java/iwishing/ccCommunity/community/controller/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
     //用根目录的时候，我们访问的时候什么都不加直接协议加端口访问，就能到这个方法，然后返回index，进入index页
     @GetMapping("/")
     public String index(){
+        System.out.println("index请求");
         return "index";
     }
 }
